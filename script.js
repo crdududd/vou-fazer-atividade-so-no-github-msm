edc2a4cfc111cbecf447658514cbd57ad417036f
crdududd <70445596+crdududd@users.noreply.github.com>
2020-10-28 16:41:51 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+let numero = prompt('digite sua idade')
+
+numero =  parseInt(numero);
+
+if (numero > 18) {
+    alert('entra ae');
+}
+
+else if (numero < 18) {
+    alert(' kk, fds ban');
+}
+
+else{
+    alert(';-;')
+} 
+
